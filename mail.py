eb8baca496855c8d60dbe587178342ece90cba82
wama <36957874+april1026@users.noreply.github.com>
2023-05-17 09:57:45 +0800

    feat: add token varify & get email from query

--- mail.py
+++ mail.py
@@ -2,6 +2,7 @@ from sanic import Sanic
 from sanic.response import json
 from sanic_mail import Sanic_Mail
 from env import MAIL_SENDER, MAIL_SENDER_PASSWORD, MAIL_SEND_HOST, MAIL_SEND_PORT, MAIL_TLS, MAIL_START_TLS, MAIL_HOST, MAIL_PORT
+from database import TOKEN
 
 app = Sanic(__name__)
 app.config.update({
@@ -17,12 +18,16 @@ sender = Sanic_Mail(app)
 
 @app.get('/mail/send')
 async def send(request):
+    if request.args.get("token") != TOKEN :
+        return json({"result": "401 Unauthorized"})
+    if request.args.get("email") == None:
+        return json({"result": "400 Bad Request"})
     await request.app.ctx.send_email(
-        targetlist="test@gmail.com",
-        subject="測試傳送",
-        content="測試傳送uu"
+        targetlist = request.args.get("email"),
+        subject = "測試傳送",
+        content = "測試傳送uu"
     )
-    return json({"result": "ok"})
+    return json({"result": "200 OK"})
 
 
 if __name__ == "__main__":
