e22136cece3fcd9f679a4ffb534d26824e0fcfe0
pudding0803 <nakohui160@gmail.com>
2023-05-18 18:08:32 +0800

    add sign-up and log-in error returning

--- app.py
+++ app.py
@@ -1,4 +1,6 @@
-from sanic import Sanic
+from sqlite3 import IntegrityError
+
+from sanic import Sanic, json
 from sanic.response import text
 from Database import Database
 from env import HOST, PORT, DATABASE
@@ -18,12 +20,18 @@ def hello(request):
 
 @app.route("/sign_up")
 def sign_up(request):
-    return text(str(db.add_user_and_get_id("test", "test")))
+    try:
+        return text(str(db.add_user_and_get_id("test", "test")))
+    except IntegrityError:
+        return json({"error": "Account already exists"}, status=409)
 
 
 @app.route("/log_in")
 def log_in(request):
-    return text(str(db.login_and_get_id("test", "test")))
+    try:
+        return text(str(db.login_and_get_id("test", "test")))
+    except:
+        return json({"error": "Invalid account or password"}, status=401)
 
 
 if __name__ == "__main__":
